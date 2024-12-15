15e5332045779a905eed30ad15055b43a00776db
SwirX <swirxytb@gmail.com>
2024-12-15 18:18:48 +0100

    synonyms/antonyms update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"83bacfc52569459a4a654727cad2546820cb0d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2313745141"
+    serviceWorkerVersion: "2470947341"
   }
 });
